9d354eb13d340ddfd402fc276a0f446e6459a440
Nosca <zhirnovvladislav99@gmail.com>
2026-09-05 18:55:53 +0300

    hands and anims

--- hands.gd
+++ hands.gd
@@ -6,6 +6,25 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+@onready var desired_shoulder_position = $ShoulderCenter.position
+@onready var desired_shoulder_rotation = $ShoulderCenter.quaternion
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	rotation.y = $"../Camera".rotation.y
+	
+	$Shoulders.position = $Shoulders.position.lerp(desired_shoulder_position, delta * 5)
+	$Shoulders.quaternion = $Shoulders.quaternion.slerp(desired_shoulder_rotation, delta * 5)
+	
+	desired_shoulder_position = $ShoulderCenter.position
+	desired_shoulder_rotation = $ShoulderCenter.quaternion
+	var swing_dir = $"../SwordCenter".swing_dir
+	var factor = swing_dir.dot(Vector2.DOWN)
+	if factor > 0 and ($"../SwordCenter".wind or $"../SwordCenter".swing):
+		var sign = sign(swing_dir.x)
+		if sign == 0:
+			sign = 1
+		var leanpos = Vector3(sign * $ShoulderLean.position.x, $ShoulderLean.position.y, $ShoulderLean.position.z)
+		var leanrot = Quaternion(sign * $ShoulderLean.quaternion.x, $ShoulderLean.quaternion.y, $ShoulderLean.quaternion.z, sign * $ShoulderLean.quaternion.w).normalized() 
+		desired_shoulder_position= $ShoulderCenter.position + (leanpos - $ShoulderCenter.position) * factor
+		desired_shoulder_rotation = $ShoulderCenter.quaternion.slerp(leanrot, factor)
--- sword_swing.gd
+++ sword_swing.gd
@@ -1,60 +1,79 @@
 extends Marker3D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 var player_rotation #angle to +y, inditcating where player is looking
-var swing_angle # angle to horizon from which the swing comes
-var swing_state_angle = 0 #how far are we into the swing 
+var swing_dir = Vector2(0,1) # direction from which the swing comes
+var swing_state = 0 #how far are we into the swing 
 
 var swing: bool = false
+var wind: bool = false
 
-var swing_axis_q = Quaternion(Vector3.FORWARD, 0)
-var swing_axis_q_real = Quaternion(Vector3.FORWARD, 0).normalized()
+var windup_accum = 0
+var windup_time = 1.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var winddown_time = 1.0
+var winddown_accum = winddown_time
+
+var swing_dir_q = Quaternion(Vector3.FORWARD, 0)
+
 func _process(delta: float) -> void:
 	
 	var y_axis_q = Quaternion(Vector3.UP, $"../Camera".rotation.y)
 	var x_axis_q = Quaternion(Vector3.RIGHT, $"../Camera".rotation.x)
 	
-	
-	var swing_state_q = Quaternion(Vector3.UP, swing_state_angle)
-	
-	quaternion = y_axis_q * x_axis_q * swing_axis_q #* swing_state_q
+	quaternion = y_axis_q * x_axis_q
+	#winddown_accum += delta
+	#winddown_accum = min(winddown_accum, winddown_time)
+	#quaternion *= swing_dir_q.slerp(Quaternion(Vector3.FORWARD, 0), winddown_accum/winddown_time)
+	#needs pos interpoliation instead
 	
 	if Input.is_action_just_pressed("LMB"):
-		swing_angle = $"../Crosshair".get_direction().angle_to(Vector2.RIGHT)
-		swing_axis_q = Quaternion(Vector3.FORWARD, -swing_angle).normalized()
+		swing_dir = $"../Crosshair".get_direction()
+		swing_dir_q = Quaternion(Vector3.FORWARD, -swing_dir.angle_to(Vector2.RIGHT)).normalized()
 		$"../AnimationPlayer".play("wind")
+		wind = true
+		windup_accum = 0
 	
-	#if $"../AnimationPlayer".is_playing():
-		#swing_axis_q_real = swing_axis_q_real.normalized().slerp(swing_axis_q, 100.0)
+	if wind:
+		windup_accum += delta;
+		quaternion *= Quaternion(Vector3.FORWARD, 0).slerp(swing_dir_q, windup_accum/windup_time)
+		#needs pos interpoliation instead
 		
 	
 	if swing:
-		quaternion *= swing_axis_q_real * swing_state_q
-		swing_state_angle += 5.0 * delta
-		if swing_state_angle >= deg_to_rad(120):
-			swing_axis_q_real = Quaternion(Vector3.FORWARD, 0).normalized()
-			swing_state_angle = 0
-			swing = false
+		var swing_state_q = Quaternion(Vector3.UP, swing_state)
+		quaternion *= swing_dir_q * swing_state_q
+		swing_state += 5.0 * delta
+		if swing_state >= deg_to_rad(150):
+			stop_swing()
+			
 
+func stop_swing():
+	#swing_dir_q = Quaternion(Vector3.FORWARD, 0).normalized()
+	swing_state = 0
+	swing = false
+	winddown_accum = 0
+	$"../AnimationPlayer".play("RESET")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	swing = true
+	if(anim_name == "wind"):
+		swing = true
+		wind = false
 
 
 func _on_blade_area_entered(area: Area3D) -> void:
-	print(area.name)
-	var packed_scene = preload("res://blood_spatter.tscn")
-	var instance = packed_scene.instantiate()
-	add_child(instance)
-	var col = $SwordGrip/Blade/RayCast3D.get_collision_point()
-	print(col)
-	instance.top_level = true
-	instance.global_position = col
-	instance.quaternion = quaternion
+	if area.get_collision_layer_value(2): # Flesh. Cringe magic number check
+		var packed_scene = preload("res://blood_spatter.tscn")
+		var instance = packed_scene.instantiate()
+		add_child(instance)
+		var col = $SwordGrip/Blade/RayCast3D.get_collision_point()
+		instance.top_level = true
+		instance.global_position = col
+		instance.quaternion = quaternion
+
+func _on_blade_body_entered(body: Node3D) -> void:
+	if body.get_collision_layer_value(1):
+		stop_swing()
